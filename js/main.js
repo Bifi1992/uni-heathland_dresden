a9a03bba3e55d5480a717216f5bc106d51becbbc
Yves Brisch <y.brisch@YvesBrischsMBP.fritz.box>
2017-11-29 19:41:52 +0100

    implemented bootstrap functionality and first modal

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var ticking = false;
 var isFirefox = (/Firefox/i.test(navigator.userAgent));
 var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
-var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
+var scrollSensitivitySetting = 60; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive)
 var slideDurationSetting = 600; //Amount of time for which slide is "locked"
 var currentSlideNumber = 0;
 var totalSlideNumber = $(".background").length;
@@ -68,7 +68,7 @@ var displayCountdown = function() {
   // time until 12th May 2018
   var endDate =  new Date("2018-05-12T12:00:00.000Z");
   var seconds = (endDate.getTime() - new Date().getTime()) / 1000;
-  var clock =  $('.countdown').FlipClock(seconds,{
+  var clock =  $('.countdown-container').FlipClock(seconds,{
     countdown: true,
     autoStart: false,
     clockFace: 'DailyCounter',
