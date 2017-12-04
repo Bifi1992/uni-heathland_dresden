8a4406907a39fca816544a51559b33dce18540fe
tonster <tony.marc@gmx.net>
2017-12-05 00:41:27 +0100

    [Bugfix] Improved image alignment.

--- include/modals/saturation-memories.php
+++ include/modals/saturation-memories.php
@@ -2,17 +2,26 @@
      aria-hidden="true">
   <div class="modal-dialog modal-xs" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="saturationMemoriesTitle">The Saturation of Memories</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
 
-          <img class="modalImg" src="../../img/saturation.jpg">
+        <div class="modal-header">
+            <div class="container-fluid">
+                <div class="row d-flex align-items-start justify-content-end">
 
-          <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=2526130417/size=small/bgcol=333333/linkcol=0f91ff/artwork=none/transparent=true/" seamless><a href="http://fateyedive-records.bandcamp.com/track/dancing-shapes">Dancing Shapes by The Saturation Of Memories</a></iframe>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
 
-      </div>
+                    <img class="modalImg" src="../../img/saturation.jpg">
+
+                    <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=2526130417/size=small/bgcol=333333/linkcol=0f91ff/artwork=none/transparent=true/" seamless><a href="http://fateyedive-records.bandcamp.com/track/dancing-shapes">Dancing Shapes by The Saturation Of Memories</a></iframe>
+
+                </div>
+                <div class="row">
+                    <h5 class="modal-title" id="saturationMemoriesTitle">The Saturation of Memories</h5>
+                </div>
+            </div>
+        </div>
+
       <div class="modal-body">
         <div class="container-fluid">
           <div class="row">
