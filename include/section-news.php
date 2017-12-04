cb4313d7fa4d03394d265c71dee9bf078a1e305c
tonster <tony.marc@gmx.net>
2017-12-04 16:00:11 +0100

    [Task] Improved Twitter widget position and timeline length.

--- include/section-news.php
+++ include/section-news.php
@@ -5,9 +5,11 @@
   <div class="p-5 placeholder"></div>
   <div class="d-flex flex-row container-twit-inst align-self-center">
     <!--Twitter widget-->
-    <a class="twitter-timeline" href="https://twitter.com/heathlandfolk" data-tweet-limit="5">Tweets by Heathland
+      <div class="twitterDiv">
+    <a class="twitter-timeline" href="https://twitter.com/heathlandfolk" data-tweet-limit="2">Tweets by Heathland
       Dresden</a>
     <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+      </div>
     <!-- InstaWidget -->
     <a href="https://instawidget.net/v/user/heathlanddresden"
        id="link-7697f5ea3edec7f7851fb5291d97614951635f34ee1634b770a11a91e8f632db">@heathlanddresden</a>
