c49df625a2baf33745b4706ba2fd1ae37743fb77
tonster <tony.marc@gmx.net>
2017-12-07 10:48:49 +0100

    [Task] Added Ageless River song and cover image.

--- include/modals/ageless-river.php
+++ include/modals/ageless-river.php
@@ -2,12 +2,27 @@
      aria-hidden="true">
   <div class="modal-dialog modal-xs" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="mr -auto modal-title" id="agelessRiverTitle">Ageless River</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
+
+        <div class="modal-header">
+            <div class="container-fluid">
+                <div class="row d-flex align-items-start justify-content-end">
+
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
+                    <img class="modalImg" src="../../img/agelessRiver.jpg">
+
+                    <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=3461394814/size=small/bgcol=333333/linkcol=0f91ff/artwork=none/transparent=true/" seamless><a href="http://fateyedive-records.bandcamp.com/track/nameless-ocean">Nameless Ocean by Ageless River</a></iframe>
+
+                </div>
+                <div class="row">
+                    <h5 class="modal-title" id="lauraMcnealTitle">Ageless River</h5>
+                </div>
+            </div>
+        </div>
+
       <div class="modal-body">
         <div class="container-fluid">
           <div class="row">
