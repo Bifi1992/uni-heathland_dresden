8238582111a618e43c1fad737ad8dbf30a15237d
tonster <tony.marc@gmx.net>
2017-12-09 15:05:40 +0100

    [Task] Added song and cover for Seventy-Three Keys.

--- include/modals/seventy-three-keys.php
+++ include/modals/seventy-three-keys.php
@@ -2,16 +2,30 @@
      aria-hidden="true">
   <div class="modal-dialog modal-xs" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="seventyThreeTitle">Seventy-Three Keys</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
+
+        <div class="modal-header">
+            <div class="container-fluid">
+                <div class="row d-flex align-items-start justify-content-end">
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
+                    <img class="modalImg" src="../../img/seventyThreeKeys.jpg">
+
+                    <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=2933736318/size=small/bgcol=333333/linkcol=0f91ff/artwork=none/transparent=true/" seamless><a href="http://fateyedive-records.bandcamp.com/track/one-with-the-rain">One with the rain by Seventy-Three Keys</a></iframe>
+
+                </div>
+                <div class="row">
+                    <h5 class="modal-title" id="saturationMemoriesTitle">Seventy-Three Keys</h5>
+                </div>
+            </div>
+        </div>
+
       <div class="modal-body">
         <div class="container-fluid">
           <div class="row">
-              Moderner Groove trifft auf oldschool Folk. Seventy-Three Keys intensiver Genremix erinnert an eine Symbiose aus Massive Attack und Neil Young.
+              Moderner Groove trifft auf oldschool Folk. Seventy-Three Keys' intensiver Genremix erinnert an eine Symbiose aus Massive Attack und Neil Young.
           </div>
         </div>
       </div>
