43cfb9b865ff3a5a5060b652523187ffcbf00858
tonster <tony.marc@gmx.net>
2017-12-22 19:56:53 +0100

    [Task] Added image and song for The Sgt. Peppers.

--- include/modals/sgt-peppers.php
+++ include/modals/sgt-peppers.php
@@ -2,12 +2,26 @@
      aria-hidden="true">
   <div class="modal-dialog modal-xs" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="sgtPeppersTitle">The Sgt. Peppers</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
+
+        <div class="modal-header">
+            <div class="container-fluid">
+                <div class="row d-flex align-items-start justify-content-end">
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
+                    <img class="modalImg" src="../../img/sgtPeppers.jpg">
+
+                    <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=3170113428/size=small/bgcol=333333/linkcol=0f91ff/artwork=none/transparent=true/" seamless><a href="http://fateyedive-records.bandcamp.com/track/land-of-stone">Land of Stone by The Sgt. Peppers</a></iframe>
+
+                </div>
+                <div class="row">
+                    <h5 class="modal-title" id="sgtPeppersTitle">The Sgt. Peppers</h5>
+                </div>
+            </div>
+        </div>
+
       <div class="modal-body">
         <div class="container-fluid">
           <div class="row">
